f4e5afeb0aa94d40e6a576f0931c3024856cd9f0
Anshul Aggarwal <aggarwalanshul1997@gmail.com>
2023-03-17 00:07:22 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,85 +1,119 @@
-var pipe1_hg;
-var hole1_hg;
-var pipe2_hg;
+let pipe1_hg, hole1_hg, pipe2_hg, pipe1b_hg, hole1b_hg, pipe2b_hg;
+let pipe1 = document.getElementById("pipe1");
+let pipe2 = document.getElementById("pipe2");
+let pipe1b = document.getElementById("pipe1b");
+let pipe2b = document.getElementById("pipe2b");
+var elem = document.getElementById("bird");
 
 setInterval(() => {
-    pipe1_hg=Math.floor(Math.random()*10)+30;
-    hole1_hg=Math.floor(Math.random()*20)+20;
-    document.getElementById("pipe1").style.height=pipe1_hg+"%";
-    document.getElementById("pipe2").style.top=pipe1_hg+hole1_hg+"%";
-    document.getElementById("pipe2").style.height=100-(pipe1_hg+hole1_hg)+"%";
-
-}, 4000);
-
-var elem=document.getElementById("bird");
-
-//gravity functionality
+  pipe1_hg = Math.floor(Math.random() * 10) + 30;
+  hole1_hg = Math.floor(Math.random() * 20) + 20;
+  pipe1.style.height = pipe1_hg + "%";
+  pipe2.style.top = pipe1_hg + hole1_hg + "%";
+  pipe2.style.height = 100 - (pipe1_hg + hole1_hg) + "%";
+}, 3000);
 
 setInterval(() => {
-    var x=parseInt(window.getComputedStyle(elem).getPropertyValue("top"));
-    if(x<=510){
-        elem.style.top=(x+3)+"px";
-    }
-    else{
-        alert("You Lost !! your score is: "+score);
-        elem.style.top=100+"px";
-        window.location.reload();
-    }
+  pipe1b_hg = Math.floor(Math.random() * 10) + 30;
+  hole1b_hg = Math.floor(Math.random() * 20) + 20;
+  pipe1b.style.height = pipe1b_hg + "%";
+  pipe2b.style.top = pipe1b_hg + hole1b_hg + "%";
+  pipe2b.style.height = 100 - (pipe1b_hg + hole1b_hg) + "%";
+}, 3000);
+
+// //gravity functionality
+setInterval(() => {
+  var x = parseInt(window.getComputedStyle(elem).getPropertyValue("top"));
+  if (x <= 670) {
+    elem.style.top = x + 3 + "px";
+  } else {
+    alert("You Lost !! your score is: " + score);
+    elem.style.top = 100 + "px";
+    window.location.reload();
+  }
 }, 30);
 
-
 //fly functionality
-
-function jump(){
-    var fly=parseInt(window.getComputedStyle(elem).getPropertyValue("top"));
-    if(fly>=14){
-        elem.style.top=(fly-40)+"px";
-    }
+function jump() {
+  var fly = parseInt(window.getComputedStyle(elem).getPropertyValue("top"));
+  if (fly >= 14) {
+    elem.style.top = fly - 50 + "px";
+  }
 }
 
-document.addEventListener('keyup', event =>{
-    if(event.code==='Space'){
-        jump();
-    }
-})
-
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    jump();
+  }
+});
 
 //score functionality
-
-var score=0;
+var score = 0;
 setInterval(() => {
-    score++;
-    document.getElementById("scr").innerHTML=score;
+  score++;
+  document.getElementById("scr").innerHTML = score;
 }, 500);
 
-
-//obstacle functionality
-
-function checkcollision(elm1,elm2){
-    var elm1Rect=elm1.getBoundingClientRect();
-    var elm2Rect=elm2.getBoundingClientRect();
-
-    return(elm1Rect.right >= elm2Rect.left && 
-        elm1Rect.left <= elm2Rect.right) && 
-        (elm1Rect.bottom>=elm2Rect.top && 
-            elm1Rect.top<=elm2Rect.bottom);
+// //obstacle functionality
+function checkcollision(elm1, elm2) {
+  let elm1Rect = elm1.getBoundingClientRect();
+  let elm2Rect = elm2.getBoundingClientRect();
+  return (
+    elm1Rect.right >= elm2Rect.left &&
+    elm1Rect.left <= elm2Rect.right &&
+    elm1Rect.bottom >= elm2Rect.top &&
+    elm1Rect.top <= elm2Rect.bottom
+  );
 }
 
 setInterval(() => {
-    if(checkcollision(document.getElementById("bird"),document.getElementById("pipe1"))){
-        elem.style.top=513+"px";
-        setTimeout(() => {
-            alert("You Lost !! Your Score is: "+score);
-            return;
-        }, 10);
-        window.location.reload();
-    }
-    else if(checkcollision(document.getElementById("bird"),document.getElementById("pipe2"))){
-        elem.style.top=513+"px";
-        setTimeout(() => {
-            alert("You Lost!! Your Score is: "+score);
-            return;
-        }, 10);
-        window.location.reload();
-    }
+  if (
+    checkcollision(
+      document.getElementById("bird"),
+      document.getElementById("pipe1")
+    )
+  ) {
+    elem.style.top = 100 + "%";
+    setTimeout(() => {
+      alert("You Lost !! Your Score is: " + score);
+      return;
+    }, 10);
+    window.location.reload();
+  } else if (
+    checkcollision(
+      document.getElementById("bird"),
+      document.getElementById("pipe1b")
+    )
+  ) {
+    elem.style.top = 100 + "%";
+    setTimeout(() => {
+      alert("You Lost !! Your Score is: " + score);
+      return;
+    }, 10);
+    window.location.reload();
+  } else if (
+    checkcollision(
+      document.getElementById("bird"),
+      document.getElementById("pipe2")
+    )
+  ) {
+    elem.style.top = 100 + "%";
+    setTimeout(() => {
+      alert("You Lost!! Your Score is: " + score);
+      return;
+    }, 10);
+    window.location.reload();
+  } else if (
+    checkcollision(
+      document.getElementById("bird"),
+      document.getElementById("pipe2b")
+    )
+  ) {
+    elem.style.top = 100 + "%";
+    setTimeout(() => {
+      alert("You Lost!! Your Score is: " + score);
+      return;
+    }, 10);
+    window.location.reload();
+  }
 }, 100);
